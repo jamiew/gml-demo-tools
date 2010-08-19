0556c8ce558576d9857a4b5b0ca063b259f157a2
Jamie Wilkinson <jamie@tramchase.com>
2010-08-19 11:14:05 -0700

    clean up and document the latest.gml downloader

--- gml_demo_downloader.sh
+++ gml_demo_downloader.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+# 
+# GML Demo Tools (Mac)
+#
+# This script downloads the latest GML tag from 000000book.com
+# then uses AppleScript to refresh 3 applications:
+#
+# - Graffiti Analysis
+# - Safari (for canvasplayer, TokiWoki player or others)
+# - Firefox4 (for the WebGL GML demo)
+# 
 
-SNOOZE="1"
+FREQUENCY="2" # seconds
+
 GA_DIR="/Applications/graffiti_analysis"
 DIR="$GA_DIR/data/tags"
 TAG="latest"
-URL="http://000000book.com/data/$TAG" # sans extension pls
-
-# TODO -- wrap all this in a fuckin loop yo
+URL="http://000000book.com/data/$TAG"
 
 # Check if the latest tag has differed
 XML="$DIR/$TAG/$TAG.xml"
@@ -14,22 +23,28 @@ XML_TEMP="$XML-temp"
 GML="$DIR/$TAG/$TAG.gml"
 
 while [ 1 ]; do
-  curl --user-agent "gml_demo 0.7/lolwut" --silent "$URL.xml" > "$XML_TEMP"
+  
+  # Download the latest GML 
+  # Include our nodename+OS version as a unique-ish useragent
+  curl --user-agent "gml_demo 0.7/#{`uname -mnrs`}" --silent "$URL.xml" > "$XML_TEMP"
 
-  # If so, download the latest & fire our "restart world"
+  # If so, download the latest & fire our "restart world" osascripts
   if [ ! -e "$XML" ] || ! diff "$XML_TEMP" "$XML" >/dev/null; then
-    echo "Latest changed or no XML/GML! Downloading..."
+
+    echo "Downloading latest.gml ..."
     rm -f "$XML"
     mv "$XML_TEMP" "$XML"
     curl --silent "$URL.gml" > "$GML"
-    echo "Restarting demo applications..."
+    echo "Restarting demo applications ..."
 
-    echo "* Graffiti Analysis"
+    # Graffiti Analysis
+    echo "* Graffiti Analysis..."
     PID=$(ps aux | grep [o]penFrameworksDebug | awk '{ print $2 }')
     [ ! -z $PID ] && kill -0 $PID && kill $PID || echo "GA not running..."
     sleep 1 # ...
     open "$GA_DIR/openFrameworksDebug.app"
 
+    # Safari
     echo "* Safari..."
     osascript <<END
       tell application "Safari"
@@ -37,8 +52,8 @@ while [ 1 ]; do
         tell application "System Events" to keystroke "r" using command down
       end tell
 END
-    # sleep 1
 
+    # Firefox
     echo "* Firefox4..."
     osascript <<END
       tell application "Firefox4"
@@ -46,11 +61,10 @@ END
         tell application "System Events" to keystroke "r" using command down
       end tell
 END
-    # sleep 1
 
   else
-    echo "http://000000book.com/latest.gml - nothing to see here - sleeping for $SNOOZE seconds..."
-    sleep $SNOOZE
+    echo "http://000000book.com/latest.gml - nothing to see here - sleeping for $FREQUENCY seconds..."
+    sleep $FREQUENCY
   fi
 done
 
